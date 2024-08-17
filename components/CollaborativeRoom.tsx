b4f31c56e67b9740013ffa069411cec6858c8782
SumantK <sumant.kantamsetty@gmail.com>
2024-08-17 21:00:55 +0530

    Collaboration Room

--- components/CollaborativeRoom.tsx
+++ components/CollaborativeRoom.tsx
@@ -4,23 +4,27 @@ import { RoomProvider, ClientSideSuspense } from '@liveblocks/react/suspense'
 import Loader from './Loader'
 import Header from '@/components/Header'
 import { Editor } from '@/components/editor/Editor'
-import { SignedIn,SignedOut,UserButton,SignInButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut, UserButton, SignInButton } from '@clerk/nextjs'
+import ActiveCollaborators from './ActiveCollaborators'
 
-const CollaborativeRoom = () => {
+const CollaborativeRoom = ({roomId, roomMetadata}: CollaborativeRoomProps) => {
     return (
-        <RoomProvider id="my-room">
+        <RoomProvider id={roomId}>
             <ClientSideSuspense fallback={<Loader />}>
                 <div className='collaborative-room'>
                     <Header>
                         <div className="flex w-fit items-center justify-center gap-2">
                             <p className="documet-title">Share</p>
                         </div>
-                        <SignedOut>
-                            <SignInButton />
-                        </SignedOut>
-                        <SignedIn>
-                            <UserButton />
-                        </SignedIn>
+                        <div className="flex w-full flex-1 justify-end gap-2 sm:gap-3">
+                            <ActiveCollaborators />
+                            <SignedOut>
+                                <SignInButton />
+                            </SignedOut>
+                            <SignedIn>
+                                <UserButton />
+                            </SignedIn>
+                        </div>
                     </Header>
                     <Editor />
                 </div>
--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -32,3 +32,17 @@ export const createDocument = async ({ userId, email }: CreateDocumentParams) =>
         console.log(`Error happened while creating a room: ${error}`)
     }
 }
+
+export const getDocument = async({roomId, userId}: {roomId: string; userId: string}) =>{
+    try{
+        const room = await liveblocks.getRoom(roomId)
+        
+        // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+        // if(!hasAccess){
+        //     throw new Error('You dont have access to this document')
+        // }
+        return parseStringify(room)
+    }catch(error){
+        console.log(error)
+    }
+}
